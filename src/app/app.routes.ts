7521765e3ab03d91cfe423073a59d9adea294fbe
Abdelrahman Fathy <abdofathy883@gmail.com>
2025-04-23 18:40:29 +0200

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ComplaintsComponent } from './complaints/complaints.component';
+import { ContactComponent } from './contact/contact.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { RegisterComponent } from './register/register.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { MyAccountComponent } from './my-account/my-account.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'about-us',
+        component: AboutUsComponent
+    },
+    {
+        path: 'complaints',
+        component: ComplaintsComponent
+    },
+    {
+        path: 'contact',
+        component: ContactComponent
+    },
+    {
+        path: 'login',
+        component: LogInComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: 'my-account',
+        component: MyAccountComponent
+    },
+    {
+        path: '**',
+        component: NotfoundComponent
+    }
+];
