6536d3a83e39a56939475eb601cabad91f1fd5e0
Abdelrahman Fathy <abdofathy883@gmail.com>
2025-05-02 08:59:51 +0300

    edits on responsivness

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutUsComponent } from './about-us/about-us.component';
 import { ComplaintsComponent } from './complaints/complaints.component';
 import { ContactComponent } from './contact/contact.component';
 import { LogInComponent } from './log-in/log-in.component';
@@ -12,44 +11,40 @@ import { MapPageComponent } from './map-page/map-page.component';
 import { RegisterNurseComponent } from './register-nurse/register-nurse.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomeComponent
-    },
-    {
-        path: 'map',
-        component: MapPageComponent
-    },
-    {
-        path: 'about-us',
-        component: AboutUsComponent
-    },
-    {
-        path: 'complaints',
-        component: ComplaintsComponent
-    },
-    {
-        path: 'contact',
-        component: ContactComponent
-    },
-    {
-        path: 'login',
-        component: LogInComponent
-    },
-    {
-        path: 'register',
-        component: RegisterComponent
-    },
-    {
-        path: 'register-nurse',
-        component: RegisterNurseComponent
-    },
-    {
-        path: 'my-account',
-        component: MyAccountComponent
-    },
-    {
-        path: '**',
-        component: NotfoundComponent
-    }
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'map',
+    component: MapPageComponent,
+  },
+  {
+    path: 'complaints',
+    component: ComplaintsComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'login',
+    component: LogInComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'register-nurse',
+    component: RegisterNurseComponent,
+  },
+  {
+    path: 'my-account',
+    component: MyAccountComponent,
+  },
+  {
+    path: '**',
+    component: NotfoundComponent,
+  },
 ];
