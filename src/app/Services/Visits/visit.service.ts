92803082f1e15576d1a3c252d05d59bd766f5ed6
MahmoodAyman <mahmoudayman1542@gmail.com>
2025-05-03 09:24:49 +0300

    Just a failed try to survive

--- src/app/Services/Visits/visit.service.ts
+++ src/app/Services/Visits/visit.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Data } from '@angular/router';
 import { Observable } from 'rxjs';
+import { RequestNearNursesDTO, ServiceDto } from '../google-map.service';
 
 export interface NewVisit {
   PatientLocation: {
     lat: number;
     lng: number;
-  }
+  };
   ActualVisitDate: Data;
   Service: string;
   ScheduledDate: Date;
@@ -21,22 +22,43 @@ export interface Visit {
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class VisitService {
   private apiURL = 'http://localhost:5004/api';
-  constructor(private http: HttpClient) { }
 
-  sendVisitData(formData: NewVisit): Observable<any>{
-    return this.http.post(this.apiURL + '/visit/FindNearestNurses', + formData);
+  constructor(private http: HttpClient) {}
+
+  // Get all available services
+  getServices(): Observable<ServiceDto[]> {
+    return this.http.get<ServiceDto[]>(`${this.apiURL}/service`);
   }
 
-  getServices(){
-    return this.http.get<any>(`${this.apiURL}/Service/GetAllServices`)
+  // Create a new visit request and find nearby nurses
+  findNearestNurses(
+    request: RequestNearNursesDTO
+  ): Observable<RequestNearNursesDTO> {
+    return this.http.post<RequestNearNursesDTO>(
+      `${this.apiURL}/visit/find-nurse`,
+      request
+    );
   }
 
-  getNearestNurses(){
-    return this.http.get<any>(`${this.apiURL}/visit/FindNearestNurses`)
+  // Accept a nurse for a visit
+  acceptNurse(visitId: number, nurseId: string): Observable<any> {
+    return this.http.post(`${this.apiURL}/visit/accept-nurse-by-patient`, {
+      visitId,
+      nurseId,
+    });
+  }
+
+  // Get nearest nurses (legacy method - can be removed if not used)
+  getNearestNurses(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiURL}/visit/nearest-nurses`);
+  }
+
+  // Send visit data (legacy method - can be replaced with findNearestNurses)
+  sendVisitData(visitData: NewVisit): Observable<any> {
+    return this.http.post(`${this.apiURL}/visit`, visitData);
   }
 }
